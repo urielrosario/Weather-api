dd1f76d557aa6438199136f3ce7f84d3d8add5e9
Uriel Rosario <urielrosario1010@gmail.com>
2021-03-05 22:03:48 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Weather-api
+# Weather Api
+
+- Weather Dashboard is an application to find weather conditions of given city and 5 day forecast.
+- Used server side API to retrieve the weather data
+
+## Table of contentens
+
+- [Description](#Description)
+- [Links](#Links)
+- [Resources](#Resources)
+- [Screenshot](#Screenshot)
+
+## Description
+
+- It displays Date, Icon Image, Temperature, Humidity, Wind Speeds, UV index and any city!
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Links
+
+- Link to Github Repository: https://github.com/urielrosario/Weather-api.git
+
+- Link to deployed website: https://urielrosario.github.io/Weather-api/
+
+## Resources
+
+- https://stackoverflow.com/questions/44177417/
+- https://getbootstrap.com/docs/5.0/getting-started/introduction/
+- https://developer.mozilla.org/en-US/
+- https://www.w3schools.com/
+- Bootcamp assignments
+
+## Screenshot
+
+![screenshot](images/Screenshot.png)
--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var apiKey = "61b20c652c853ac048c8f20d419c2cd0";
 var city = $("#city");
 
+// var currentWeatherIcon =
+//   "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+// var cityUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   city +
+//   "&units=imperial&appid=" +
+//   apiKey;
+
 var currentHour = moment().hour();
 var time = moment();
 $("#time").text(time.format("dddd, MMMM Do"));
@@ -8,40 +16,50 @@ $("#time").text(time.format("dddd, MMMM Do"));
 function fetchWeather() {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
+      city.val() +
       "&appid" +
       apiKey +
       "&units=imperial"
   )
-    .then(function (response) {
-      return response.json();
+    .then(function (data) {
+      return data.json();
     })
     .then(function (data) {
       console.log(data);
-      var icon = $("#icon");
+
       var cityNames = $("#city-names");
       var temperature = $("#temperature");
       var humidity = $("#humidity");
       var windSpeed = $("#windspeed");
-      cityNames.text(data.name + time.format("MM/DD/YYYY"));
-      icon.text(data.weather[0].icon);
-      temperature.text("Temperature: " + data.main.temperature);
-      humidity.text("Humidity (%): " + data.main.humidity);
-      windSpeed.text("Wind Speed (MPH): " + data.wind.speed);
+      var icon = data.weather[0].icon;
+      var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+      $(".icon").attr("src", iconUrl);
+      cityNames.text(data.name + moment().format("MM/DD/YYYY"));
+      temperature.text(parseInt(data.main.temp) + " °F");
+      humidity.text(parseInt(data.main.humidity) + " %");
+      windSpeed.text(data.wind.speed + " MPH");
       var lt = data.coord.lat;
       var ln = data.coord.lon;
       fetchUv(lt, ln);
+
+      var updateCity = [];
+      updateCity = JSON.parse(localStorage.getItem("city"));
+      if (updateCity !== null) {
+        updateCity = [];
+        updateCity.push(city.val());
+        localStorage.setItem("city", JSON.stringify(updateCity));
+      }
     });
 }
 
 function fetchUv(lt, ln) {
   fetch(
-    "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+      apiKey +
+      "&lat" +
       lt +
       "&lon" +
-      ln +
-      "&appid=" +
-      apiKey
+      ln
   )
     .then(function (response) {
       return response.json();
@@ -49,9 +67,10 @@ function fetchUv(lt, ln) {
     .then(function (data) {
       console.log(data);
       var uvIndex = $("#uv");
-      uvIndex.text(data.value);
+      uvIndex.text("UV Index: " + data.value);
     });
 }
+// 5day forecast cards
 function fetchForecast() {
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -65,12 +84,55 @@ function fetchForecast() {
     })
     .then(function (data) {
       console.log(data);
-      var date0 = $("#date0");
-      var humidity0 = $("cardHumidity0");
-      var temp0 = $("#cardtemp0");
-      date0.text(moment().add(1, "d").format("MM/DD/YYYY"));
-      temp0.text("Temperature:" + data.list[3].main.humidity);
+
+      var day1 = $("#date1");
+      day1.text(moment().add(1, "day").format("MM/DD/YYYY"));
+      var temp1 = $("#cardTemp1");
+      temp1.text(data.list[3].main.temp + " °F");
+      var humid1 = $("#cardHumidity1");
+      humid1.text(data.list[3].main.humidity + " %");
+
+      var day2 = $("#date2");
+      day2.text(moment().add(2, "days").format("MM/DD/YYYY"));
+      var temp2 = $("#cardTemp2");
+      temp2.text(data.list[11].main.temp + " °F");
+      var humid2 = $("#cardHumidity2");
+      humid2.text(data.list[11].main.humidity + " %");
+
+      var day3 = $("#date3");
+      day3.text(moment().add(3, "days").format("MM/DD/YYYY"));
+      var temp3 = $("#cardTemp3");
+      temp3.text(data.list[19].main.temp + " °F");
+      var humid3 = $("#cardHumidity3");
+      humid3.text(data.list[19].main.humidity + " %");
+
+      var day4 = $("#date4");
+      day4.text(moment().add(4, "days").format("MM/DD/YYYY"));
+      var temp4 = $("#cardTemp4");
+      temp4.text(data.list[27].main.temp + " °F");
+      var humid4 = $("#cardHumidity4");
+      humid4.text(data.list[27].main.humidity + " %");
+
+      var day5 = $("#date5");
+      day5.text(moment().add(5, "days").format("MM/DD/YYYY"));
+      var temp5 = $("#cardTemp5");
+      temp5.text(data.list[35].main.temp + " °F");
+      var humid5 = $("#cardHumidity5");
+      humid5.text(data.list[35].main.humidity + " %");
     });
 }
 
-("https://api.openweathermap.org/data/2.5/weather?q=");
+// function keepSearch(event) {
+//   $(listEl).attr("class", "list-group-item");
+
+//   var listEl = event.target;
+//   if (event.target.matches("li")) {
+//     savedcity - listEl.textContent.trim();
+//     fetchWeather(savedcity);
+//   }
+
+$("#search-button").on("click", function (event) {
+  event.preventDefault();
+  fetchWeather();
+  fetchForecast();
+});
